d8b178ccc58b03c8742a261fc5a025767b2426e1
DungLe2912 <qdung.le3912@gmail.com>
2019-04-12 21:35:23 +0700

    Luu lai de test code co hu thi backup

--- js/myJSindex.js
+++ js/myJSindex.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();   
+  $(".dropdown").mouseenter(function(){
+  	$("#dropdown_megamenu").fadeIn(1200);
+  });
+  $(".dropdown").mouseleave(function(){
+  	$("#dropdown_megamenu").fadeOut();
+  });
 });
