3389dff7d2c62e5e54b84ede0d928cc82976e240
DungLe2912 <qdung.le3912@gmail.com>
2019-04-13 21:19:43 +0700

    Tao xong sidebar, sua duoc loi fixed-top cua nav-menu

--- js/myJSindex.js
+++ js/myJSindex.js
@@ -6,7 +6,23 @@ $(document).ready(function(){
   $(".dropdown").mouseleave(function(){
   	$("#dropdown_megamenu").fadeOut();
   });
+  $(".openbtn").click(function() {
+     $("#main").addClass('prevent-click');
+      $("#nav_fixed").removeClass('sticky'); 
+  });
+   $(".closebtn").click(function() {
+     $("#main").removeClass('prevent-click');
+     $("#nav_fixed").addClass('sticky');
+  });
+  $("#nav_fixed").addClass('sticky');
+   $('.abc').click(function (e) {  
+        $('.abc').removeClass('selected');  
+        $(this).addClass('selected');  
+    });  
+  	
 });
+
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
