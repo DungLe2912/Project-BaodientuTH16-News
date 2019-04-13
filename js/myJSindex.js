f095e347deaf458cc47e53991ec228bca074afb0
DungLe2912 <qdung.le3912@gmail.com>
2019-04-13 12:10:55 +0700

    Loi tum lum. Ai do fix gium cai fixed-top cua nav-menu di

--- js/myJSindex.js
+++ js/myJSindex.js
@@ -6,8 +6,13 @@ $(document).ready(function(){
   $(".dropdown").mouseleave(function(){
   	$("#dropdown_megamenu").fadeOut();
   });
-   $('#sidebarCollapse').on('click', function () {
-	     $('#sidebar').toggleClass('active');
-	     $(this).toggleClass('active');
-    });
 });
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
